3e6a1c75d98162cc082f845ede90131d02c176f6
Igor Tsimbalyuk KI-17-1 <igor.tsimbalyuk5@gmail.com>
2021-06-05 18:40:23 +0300

    final

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-window.addEventListener('DOMContentLoaded', () => {});
+window.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.swiper-container', {
+    spaceBetween: 10,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+});
