759c561e30e3cf732b17105f748f520de4551e7a
Igor Tsimbalyuk KI-17-1 <igor.tsimbalyuk5@gmail.com>
2021-06-18 18:05:44 +0300

    finish

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,11 @@
+import Forms from './modules/form';
+import smoothScrolling from './modules/smoothScrolling';
+import burger from './modules/burger';
+
 window.addEventListener('DOMContentLoaded', () => {
+  new Forms('.form').init();
+  smoothScrolling('.nav__item[data-goto]');
+  burger('.burger', '.nav__list', '.burger__list');
   const swiper = new Swiper('.swiper-container', {
     spaceBetween: 10,
     pagination: {
